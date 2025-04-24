9e981efbeb655e03a59a2c820b10107928dd551d
BabuBubble <122726082+BabuBubble@users.noreply.github.com.>
2025-04-24 22:06:27 +0900

    ２ページ目を反映

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('index', { title: 'My Web App' });
+    res.render('index', { title: 'My Web App' });
+});
+
+router.get('/page2', (req, res) => {
+    res.render('page2');
 });
 
 module.exports = router;
