ed6b11c2abee07b04766f37e13238289066b56c8
BabuBubble <122726082+BabuBubble@users.noreply.github.com.>
2025-05-13 00:42:59 +0900

    Sign Up実装。

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const { route } = require('express/lib/application');
 const router = express.Router();
+const db = require('../db');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 router.get('/', (req, res) => {
     const is_login = req.session.user ? true : false;
@@ -9,18 +12,40 @@ router.get('/', (req, res) => {
 
 router.get('/mypage', (req, res) => {
     const is_login = req.session.user ? true : false;
-    if ( is_login ) {
-        res.render('mypage', { 
-            title: req.session.user.email,
-            login: is_login 
-        });
-    } else {
+    if ( !is_login ) {
         res.redirect('/login');
     }
+
+    const userId = req.session.user.id;
+
+    db.query('SELECT * FROM books WHERE user_id = ?', [userId], (err, results) => {
+        if (err) throw err;
+
+        res.render('mypage', { 
+            title: req.session.user.email,
+            login: is_login ,
+            books: result
+        });
+    });
+});
+
+router.get('/register', (req, res) => {
+    res.render('register');
 });
 
 router.get('/login', (req, res) => {
     res.render('login');
+});
+
+router.post('/register', async (req, res) => {
+    const { email, password } = req.body;
+
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+    db.query('INSERT INTO users (email, password) VALUES (?, ?)', [email, hashedPassword], (err, results) => {
+        if (err) throw err;
+        res.redirect('/login');
+    });
 });
 
 router.post('/login', async (req, res) => {
